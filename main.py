31d760c38d58a5a814d450cbb4a8613626d9afab
Timerlan <2504timoka@mail.ru>
2022-09-08 15:03:06 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+b, a = input("Введите первые координаты: "), input("Введите вторые коордианты: ")
+while True:
+    try:
+        image = requests.get(f"https://static-maps.yandex.ru/1.x/?ll={a},{b}&spn=0.006,0.006&l=map")
+        with open("image.png", "wb") as f:
+            f.write(image.content)
+        print("Изображение сохранено в файле image.png")
+        print()
+    except Exception:
+        print("Вы ввели неверные координаты! Введите координаты заново!")
+    finally:
+        b, a = input("Введите первые координаты: "), input("Введите вторые коордианты: ")
+
+# 47.409318, 47.252063
